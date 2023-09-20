f3cd9554e48d0b3c712adef86da3368798582d78
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-20 15:56:31 +0200

    Add handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <>
-      <h1>React Guest List</h1>
-      <div>
-        <p>Please add your first and last name to sign up:</p>
-        <input type="text" placeholder="your first name" />
-        <br /> <br />
-        <input type="text" placeholder="your first name" />
-        <br />
-        <br />
-        <button>add me as a guest</button>
-      </div>
+      <header>
+        <h1>React Guest List</h1>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p>Please add the first and last name to sign up:</p>
+            <input
+              onChange={() => {
+                setFirstName();
+              }}
+              value={firstName}
+              placeholder="first name"
+            />
+            <br /> <br />
+            <input
+              onChange={() => {
+                setLastName();
+              }}
+              value={lastName}
+              placeholder="last name"
+            />
+            <br />
+            <br />
+            <button onClick={() => setFirstName(firstName)}>
+              add as a guest
+            </button>
+            <br />
+            <br />
+          </div>
+        </form>
+        <hr />
+        <div>
+          <h2>Added guests</h2>
+          <p>
+            {firstName} {lastName}
+          </p>
+        </div>
+      </main>
     </>
   );
 }
