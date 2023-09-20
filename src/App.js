5b315f7b421ef22ab45558a643054d4ab9ce5bb5
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-20 14:45:44 +0200

    Add input field for first and last names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import './App.css';
-import logo from './logo.svg';
-
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React Guest List</h1>
+      <div>
+        <p>Please add your first and last name to sign up:</p>
+        <input type="text" placeholder="your first name" />
+        <br /> <br />
+        <input type="text" placeholder="your first name" />
+        <br />
+        <br />
+        <button>add me as a guest</button>
+      </div>
+    </>
   );
 }
