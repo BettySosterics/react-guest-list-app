c2220edd98b837fbc8df01b8805ae3996f9acb12
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-21 17:25:05 +0200

    Set up API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
+import DataFetching from './DataFetching';
 
 export default function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
-  // function handleFirstNameSubmit(event) {
-  //   setFirstName(event.currentTarget.value);
-  // }
+  const [guests, setGuests] = useState([]);
+  const [guestId, setGuestId] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // function handleLastNameSubmit(event) {
-  //   setLastName(event.currentTarget.value);
-  // }
+  async function getGuests() {
+    const baseUrl = 'http://localhost:4000';
+    const response = await fetch(`${baseUrl}/guests`);
 
-  function handleButtonSubmit() {
-    return `${firstName} ${lastName}`;
+    const allGuests = await response.json();
+
+    setGuestId(0);
+    setGuests([...guests, allGuests.body]);
+  }
+
+  useEffect(() => {
+    async function firstRenderFetch() {
+      const baseUrl = 'http://localhost:4000';
+      const response = await fetch(`${baseUrl}/guests`);
+
+      const data = await response.json();
+
+      setGuests([data.body]);
+
+      setIsLoading(false);
+    }
+
+    firstRenderFetch().catch((error) => {
+      console.log(error);
+    });
+  }, []); // triggers only on first render
+
+  useEffect(() => {
+    async function createGuest() {
+      const baseUrl = 'http://localhost:4000';
+      const response = await fetch(`${baseUrl}/guests`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName: { firstName },
+          lastName: { lastName },
+        }),
+      });
+      const createdGuest = await response.json();
+      console.log(createdGuest.body.firstName);
+
+      setGuests([...guests, createdGuest.body]);
+    }
+
+    createGuest().catch((error) => {
+      console.log(error);
+    });
+  }, [guestId]);
+
+  if (isLoading) {
+    return 'Loading...';
   }
 
   return (
@@ -38,18 +86,34 @@ export default function App() {
             placeholder="last name"
           />
 
-          <button className={styles.button} onClick={handleButtonSubmit}>
+          <button
+            className={styles.button}
+            onClick={async () => await getGuests()}
+          >
             ADD GUEST
           </button>
           <br />
         </form>
         <hr />
-        <div>
-          <section>
-            <h2>Added guests</h2>
-            <p>{handleButtonSubmit()}</p>
-          </section>
-        </div>
+
+        <section className={styles.guestList}>
+          <h2>Added guests</h2>
+          <DataFetching />
+
+          {/* <p>{handleButtonSubmit()}</p> */}
+
+          {/* <DataFetching /> */}
+
+          {guests.map((guest) => {
+            return (
+              <div key={`guest-${guest.id}`}>
+                {guest.firstName} {guest.lastName}
+                <div>Attending: {guest.attending}</div>
+                <hr />
+              </div>
+            );
+          })}
+        </section>
       </main>
     </>
   );
