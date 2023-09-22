f1fe333d9076a98ff976ad01ac693dd658779add
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-22 10:50:18 +0200

    Add Form component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// const GUEST = [
+//   { id: 'guest-0', firstName: 'Tré', lastName: 'Cool', attending: false },
+//   { id: 'guest-1', firstName: 'Mike', lastName: 'Dirnt', attending: false },
+//   {
+//     id: 'guest-2',
+//     firstName: 'Bille Joe',
+//     lastName: 'Armstrong',
+//     attending: false,
+//   },
+// ];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
